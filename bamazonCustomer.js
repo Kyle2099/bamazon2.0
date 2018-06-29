7b79a992989983d614a31470b6bc0d0d18c4fdd6
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2018-06-28 22:58:43 -0500

    file struct, create app, added function

---
+++ bamazonCustomer.js
@@ -0,0 +1,107 @@
+const mysql = require('mysql');
+const inquirer = require("inquirer");
+require("dotenv").config();
+
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.password,
+    database: 'bamazondb2',
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.log('error connecting to products');
+    }
+    inventory();
+});
+
+function inventory() {
+    connection.query('SELECT * FROM products', function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+        userStockOptions(result);
+    });
+}
+
+function wantToExit(choice) {
+    if (choice.toLowerCase() === 'b') {
+        console.log('BYEEEEE! Thanks for shopping with us.');
+        process.exit(0);
+    }
+}
+
+
+function userStockOptions(inv) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'select',
+        message: 'What item would you like to purchase?',
+        validate: function (sup) {
+            return !fog(sup) || sup.toLowerCase() === 'b';
+        }
+    }
+    ])
+        .then(function (sup) {
+            wantToExit(sup.choice);
+            let pickedId = cutNum(sup.choice);
+            let stock = invLeft(pickedId, inv);
+
+            if (products) {
+                stockQuantity(products);
+            }
+            else {
+                console.log('Sorry please check back later we are out of that item.');
+                inventory();
+            }
+        });
+}
+
+function stockQuantity(product) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'quantity',
+        message: 'How many would you like to buy?',
+        validate: function(sup) {
+            return sup > 0 || sup.toLowerCase() === 'b';
+        }
+    }
+])
+.then(function(sup) {
+    wantToExit(sup.amount);
+    let amount = cutNum(sup.amount);
+
+    if (amount > product.stock_quantity) {
+        console.log('Sorry, but we out. Check back next week!')
+        inventory();
+    }
+    else {
+        letsBuy(product, amount);
+    }
+});
+}
+
+function letsBuy(product, amount) {
+    connection.query(
+        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+        [amount, product.item_id],
+        function(err, result) {
+            console.log('Thanks you bought' + amount + ' ' + product.product_name);
+        }
+    );
+}
+
+function reviewStock(pickedID, stock) {
+    for (let i = 0; i < stock.length; i++) {
+        if (stock[i].itme_id === pickedID) {
+            return stock[i];
+        }
+    }
+    return null;
+}
+
+
---
+++ bamazonManager.js
@@ -0,0 +1,148 @@
+const mysql = require('mysql');
+const inquirer = require("inquirer");
+require("dotenv").config();
+
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.password,
+    database: 'bamazondb2',
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.log('error connecting to products');
+    }
+    inventory();
+});
+
+
+function loadBossMenu() {
+    connection.query('SELECT * FROM products', function (err, result) {
+        if (err) throw err;
+
+        loadOptions(result);
+    });
+}
+
+function loadOptions(products) {
+    inquirer.prompt({
+        type: 'list',
+        name: 'choice',
+        choices: ['See items for sale', 'Low items', 'Add to existing items', 'Add new item', 'Or you can Quit'],
+        message: 'What will it be today?'
+    })
+        .then(function (sup) {
+            switch (sup.choice) {
+                case "See items for sale":
+                    console.table(products);
+                    loadBossMenu();
+                    break;
+                case 'Low items':
+                    loadLowItems();
+                    break;
+                case 'Add to existing items':
+                    addToExistingItems(products)
+                    break;
+                case "Add new item":
+                    BossNewItem(products);
+                    break;
+                default:
+                    console.log('BYEEEEEEE!');
+                    process.exit(0);
+                    break;
+
+            }
+        });
+}
+
+function loadLowItems() {
+    conection.query('SELECT * FROM products WHERE stock_quanity <= 10', function (err, result) {
+        if (err) throw err;
+        console.table(result);
+        loadBossMenu();
+    });
+}
+
+
+function bossForHowMany(products) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'quantity',
+        message: 'How many would you like to buy?',
+        validate: function (sup) {
+            return sup > 0;
+        }
+    }
+    ])
+        .then(function (sup) {
+            let amount = cutNum(sup.amount);
+            AddItem(product, quantity);
+        });
+}
+
+function BossNewItem(products) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_name',
+            message: 'Please add in the name of the item you would like to add.'
+        },
+        {
+            type: 'list',
+            name: 'department_name',
+            choices: addedCatagories(products),
+            message: 'Which Catag. does this item belong to.'
+        },
+        {
+            type: 'input',
+            name: 'cost',
+            message: 'How much does this item cost?',
+            validate: function (sup) {
+                return sup > 0;
+            }
+        },
+        {
+            type: 'input',
+            name: 'amount',
+            message: 'How many are going into the store?',
+            validate: function (sup) {
+                return !fog(sup);
+            }
+        }
+    ])
+        .then(addNewItem)
+}
+
+function addNewItem(sup) {
+    connection.query(
+        'INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (?, ?, ?, ?)',
+        [sup.product_name, sup.department_name, sup.price, sup.quantity],
+        function (err, result) {
+            if (err) throw err;
+            console.log(sup.product_name + 'We got your new items added.');
+            loadBossMenu();
+        }
+    );
+}
+
+function addedCatagories(products) {
+    var departments = [];
+    for (var i = 0; i < products.length; i++) {
+        if (departments.indexOf(products[i].department_name) === -1) {
+            departments.push(products[i].department_name);
+        }
+    }
+    return departments;
+}
+
+function reviewStock(pickedID, stock) {
+    for (let i = 0; i < stock.length; i++) {
+        if (stock[i].itme_id === pickedID) {
+            return stock[i];
+        }
+    }
+}
